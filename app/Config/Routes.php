68a1ab31daeffa9704118a658b71c57a5456889a
nadia permata putri <nadiapermataputri807@gmail.com>
2025-07-08 11:52:49 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
+
+// Hanya jika ingin mengaktifkan routing otomatis
+$routes->setAutoRoute(true);
